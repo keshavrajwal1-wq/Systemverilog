592db6ca92501502f5669d97bb85bf2830ea450a
keshavrajwal1-wq <keshavrajwal1@gmail.com>
2026-07-17 20:01:38 +0530

    Added 2-to-1 multiplexer

---
+++ data types/2to1mux.sv
@@ -0,0 +1,21 @@
+// Code your design here
+`timescale 1ns/1ps
+
+module m2to1mux(
+  input a, b , sel ,
+  output y)
+;
+  
+   reg temp ;
+  
+  always @(*)begin 
+    if (sel ==0 )
+      temp = a;
+    else
+      temp = b;
+  end
+    
+    assign y = temp;
+    
+  
+endmodule
