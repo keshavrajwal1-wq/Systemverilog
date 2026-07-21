adb26d39db8d969765a2acb9d67be2ace433bebb
Keshav Rajwal <keshavrajwal1@gmail.com>
2026-07-21 09:23:01 +0530

    Update soltion.sv

--- class/problem statement/problem 2/soltion.sv
+++ class/problem statement/problem 2/soltion.sv
@@ -1,11 +1,19 @@
 module test();
-  
+  int unsigned exp_result = 15;
+  int unsigned cal_result;
   function integer unsigned function_name(integer unsigned a,integer unsigned b);
     return a*b;
   endfunction
     
     initial begin
-      
-      $display("value : %0d",function_name(5,3));
+      cal_result = function_name(5,3);
+      if (exp_result == cal_result) begin
+        $display("test passed");
+        end
+      else begin
+        $display("test failed");
+      end
+      $display("calculated result : %0d", cal_result);
+      $display("expected result : %0d", exp_result);
     end
                endmodule
