b675c8899f479ff012bbd7402290cfdf482be1f6
Keshav Rajwal <keshavrajwal1@gmail.com>
2026-07-22 13:23:57 +0530

    Implement task to swap values by reference

---
+++ class/task - pass by reference usuage.sv
@@ -0,0 +1,23 @@
+module tb;
+ 
+  bit [1:0] a;
+  bit[1:0] b;
+
+  //use of automatic and ref to pass by reference
+  task automatic swap(  ref bit [1:0] a, b);
+ bit [1:0]temp;
+  temp= a;
+  a = b;
+  b= temp;
+  $display(" value of a and b : %0d , %0d", a , b);
+  endtask
+
+
+initial begin 
+  a= 1;
+  b=2;
+  swap(a,b);
+  $display(" actual value of a and b : %0d %0d", a,b);
+  
+end
+  endmodule
