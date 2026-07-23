a3d0dfa3a3638748ad6a499a81b16069467a3ebf
Keshav Rajwal <keshavrajwal1@gmail.com>
2026-07-23 20:18:11 +0530

    Update task - pass by reference usuage.sv

--- class/task - pass by reference usuage.sv
+++ class/task - pass by reference usuage.sv
@@ -4,7 +4,8 @@ module tb;
   bit[1:0] b;
 
   //use of automatic and ref to pass by reference
-  task automatic swap(  ref bit [1:0] a, b);
+ task automatic swap(  ref bit [1:0] a, b);  //use of " to use global variable as reference any time a task is called the global variable changes
+ //" automatic " creates new local variable in this case "temp" , so that if multiple times a task is called temp value wont interfere to each other
  bit [1:0]temp;
   temp= a;
   a = b;
