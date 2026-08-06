4eed644c74451d3fc522922162e60427b05811cc
Keshav Rajwal <keshavrajwal1@gmail.com>
2026-08-06 12:56:43 +0530

    Create  solution.sv

---
+++ randomization/problem statements /problem 1/solution.sv
@@ -0,0 +1,18 @@
+class class_name;
+  rand bit [ 7: 0 ] x , y , z;
+  
+endclass
+
+module module_name();
+  class_name handler_name;
+  initial begin
+    handler_name = new();
+  
+  for (int i = 0 ; i <20 ; i++ ) begin
+    handler_name.randomize();
+    $display("x = %0d y = %0d z = %0d", handler_name.x , handler_name.y , handler_name.z);
+    #20;
+  end
+  end
+  
+endmodule
