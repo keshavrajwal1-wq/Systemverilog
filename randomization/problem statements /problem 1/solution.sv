52d70d69d04c971489e51c26649c7e4554ffcdff
Keshav Rajwal <keshavrajwal1@gmail.com>
2026-08-06 13:05:14 +0530

    Update solution.sv

--- randomization/problem statements /problem 1/solution.sv
+++ randomization/problem statements /problem 1/solution.sv
@@ -10,7 +10,7 @@ module module_name();
   
   for (int i = 0 ; i <20 ; i++ ) begin
     handler_name.randomize();
-    $display("x = %0d y = %0d z = %0d", handler_name.x , handler_name.y , handler_name.z);
+    $display("x = %0d y = %0d z = %0d time = %0t", handler_name.x , handler_name.y , handler_name.z, $time);
     #20;
   end
   end
